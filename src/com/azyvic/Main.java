c26151a5bf050e483c57ea451d8c16dc30898654
GreenBurn <azyamov_victor@mail.ru>
2021-04-11 20:08:16 +0300

    First

---
+++ src/com/azyvic/Main.java
@@ -0,0 +1,15 @@
+package com.azyvic;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello world!");
+	int sum = 5;
+	int sum2 = 5;
+	int infiks = --sum;
+	System.out.println(infiks);
+	while (++sum2 > 5)
+                System.out.println(sum2);
+        // write your code here
+    }
+}
