77cf746d8d638d42400365548cbf1ecab87e9130
GreenBurn <azyamov_victor@mail.ru>
2021-04-11 20:18:32 +0300

    snake input

--- src/com/azyvic/Main.java
+++ src/com/azyvic/Main.java
@@ -3,13 +3,10 @@ package com.azyvic;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world!");
-	int sum = 5;
-	int sum2 = 5;
-	int infiks = --sum;
-	System.out.println(infiks);
-	while (++sum2 > 5)
-                System.out.println(sum2);
+	int x1 = 1;
+	int y1 = 1;
+	char sym1 = '*';
+	
         // write your code here
     }
 }
